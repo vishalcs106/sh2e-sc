d49b8e778ba71636e4d9f91ec2a2d385fe981c51
Vishal <vishalcs106@rocketmail.com.com>
2024-06-22 22:50:11 +0530

    Updated hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,40 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomicfoundation/hardhat-verify");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.20",
+
+  networks: {
+    hardhat: {
+      chainId: 43114,
+      gasPrice: 225000000000,
+      forking: {
+        url: "https://api.avax.network/ext/bc/C/rpc",
+        enabled: true,
+        blockNumber: 2975762,
+      },
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/C/rpc",
+      chainId: 43113,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    avalanche: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      chainId: 43114,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSACN_PRIVATE,
+  },
+  sourcify: {
+    enabled: true,
+  },
 };
